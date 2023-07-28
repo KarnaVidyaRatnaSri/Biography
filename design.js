4b99dd0ca057a1513343dab13b73af4b37e731ea
KarnaVidyaRatnaSri <137883417+KarnaVidyaRatnaSri@users.noreply.github.com>
2023-07-28 16:01:30 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,22 @@
+ReactDOM.render(<div>
+   <img height="500" width="400"src="https://vetbossel.in/wp-content/uploads/2022/09/srushti-deshmukh-ias.jpg"/><br/>
+<h3>
+    The 27-year-old IAS Srishti Deshmukh hails from
+    Kasturba Nagar in Bhopal, Madhya Pradesh.<br/>
+    His name is Srushti Jayant Deshmukh.
+    She achieved IAS AIR 5 in the 2018 UPSC CSE test.<br/> 
+    She became an IAS officer when the result was declared
+    in 2019.<br/><br/>
+    She cleared the UPSC exam in 2018 with 5th rank.<br/>
+    IAS Srishti belongs to the 2019 IAS Batch,
+    and her husband also belongs to the same batch.<br/> 
+    Both of them are given the Madhya Pradesh cadre.
+    Recently appointed in the Narsingpur District of<br/>
+    Madhya Pradesh is Srushti Deshmukh, who finished
+    fifth in the 2018 UPSC.
+</h3>
+<h1>Srushti Education details</h1>
+<h3>***Srushti Deshmukh completed her schooling at the Bhavan's BP Vidya Mandir in Amravati,Maharashtra.<br/><br/>
+    ***She then pursued a Bachelor of
+    Engineering in Chemical Engineering from the
+    Rajiv Gandhi Institute of Technology in Mumbai.</h3></div>,document.getElementById("biography"));
